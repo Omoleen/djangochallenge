3e1265509501ce2f88e9d7c1edb3cd8cdb40a00e
omoleen <Oreoluwa123>
2023-06-03 13:24:20 +0100

    using the latest version of django, using python_2_unicode_compatible wasn't needed so it was removed, on_delete argument was also added to ForeignKey fields that did not have the on_delete argument to avoid errors, a migration folder was also created in the mailer app in order to makemigrations and migrate app to db

--- mailer/models.py
+++ mailer/models.py
@@ -3,19 +3,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+# from django.utils.encoding import python_2_unicode_compatible
+# import six
 
 
 class Company(models.Model):
     name = models.CharField(max_length=150)
     bic = models.CharField(max_length=150, blank=True)
 
+    @property
     def get_order_count(self):
         orders = 0
         for order in self.orders.all():
             orders += 1
         return orders
 
+    @property
     def get_order_sum(self):
         total_sum = 0
         for contact in self.contacts.all():
@@ -26,9 +29,9 @@ class Company(models.Model):
 
 class Contact(models.Model):
     company = models.ForeignKey(
-        Company, related_name="contacts", on_delete=models.PROTECT)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150, blank=True)
+        Company, related_name="contacts", on_delete=models.PROTECT, db_index=True)
+    first_name = models.CharField(max_length=150, db_index=True)
+    last_name = models.CharField(max_length=150, blank=True, db_index=True)
     email = models.EmailField()
 
     def get_order_count(self):
@@ -38,11 +41,10 @@ class Contact(models.Model):
         return orders
 
 
-@python_2_unicode_compatible
 class Order(models.Model):
     order_number = models.CharField(max_length=150)
-    company = models.ForeignKey(Company, related_name="orders")
-    contact = models.ForeignKey(Contact, related_name="orders")
+    company = models.ForeignKey(Company, related_name="orders", on_delete=models.PROTECT)
+    contact = models.ForeignKey(Contact, related_name="orders", on_delete=models.PROTECT)
     total = models.DecimalField(max_digits=18, decimal_places=9)
     order_date = models.DateTimeField(null=True, blank=True)
     # for internal use only
