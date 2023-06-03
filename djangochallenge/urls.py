440f5eeaa571932d68ca4d4030d384fd3d4acfcc
omoleen <Oreoluwa123>
2023-06-03 13:35:38 +0100

    Refactored imports and implemented caching for improved performance
    Replaced include and url imports with re_path and path imports from django.urls in the urls.py files of the project and the mailer app. Also, updated the index.html template in the mailer app to use load static instead of load staticfiles. Furthermore, wrapped the IndexView class with the cache_page class in the mailer app's urls.py file to enable caching and reduce page load time on subsequent refreshes.

--- djangochallenge/urls.py
+++ djangochallenge/urls.py
@@ -1,17 +1,18 @@
 #-*- coding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls import include, url
+# from django.conf.urls import include
+from django.urls import include, re_path
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include("mailer.urls")),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^', include("mailer.urls")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
--- mailer/urls.py
+++ mailer/urls.py
@@ -1,8 +1,12 @@
 #-*- coding: utf-8 -*-
-from django.conf.urls import include, url
+# from django.conf.urls import include
+from django.urls import path, re_path
 
 from mailer.views import IndexView
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
+
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name="index"),
+    path('', cache_page(60 * 60)(IndexView.as_view()), name="index"),  # Cache for 60 minutes
 ]
