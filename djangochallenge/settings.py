313e82a44013609438e41bc0efbfab490ad44fbe
omoleen <Oreoluwa123>
2023-06-03 13:29:42 +0100

    MIDDLEWARE_CLASSES was changed to MIDDLEWARE in settings.py, TEMPLATES variable was also added to settings.py, templates and static folders were created in the root of the project, DEFAULT_AUTO_FIELD and CACHE_MIDDLEWARE_ENABLED were also initialized in the settings.py, and * was added to ALLOWED_HOSTS

--- djangochallenge/settings.py
+++ djangochallenge/settings.py
@@ -4,12 +4,13 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = '#3(%x30j#h6c4l$g2fiknup^x21vw#&&4a%_pbowzs5eh&e%q#'
 
 DEBUG = True
 TEMPLATE_DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 INTERNAL_IPS = ['127.0.0.1', '0.0.0.0']
 
 INSTALLED_APPS = (
@@ -23,7 +24,24 @@ INSTALLED_APPS = (
     'mailer',
 )
 
-MIDDLEWARE_CLASSES = (
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -31,7 +49,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+]
 
 ROOT_URLCONF = 'djangochallenge.urls'
 WSGI_APPLICATION = 'djangochallenge.wsgi.application'
@@ -42,11 +60,13 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'djangochallenge.sqlite3'),
     }
 }
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHE_MIDDLEWARE_ENABLED = True
